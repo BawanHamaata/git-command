8ff5987b7e86f512a4f6fe0b075ff95cac98890c
bawan <brwa.test1@gmail.com>
2024-03-26 22:29:03 +0300

    sarata

---
+++ map=[.py
@@ -0,0 +1,88 @@
+map={
+    5: [3,7],
+    3:[2,4],
+    7:[8],
+    2:[],
+    4:[8],
+    8:[]#goal
+}
+def bfs(start,goal):
+    box=[start]
+    expored_set=[]
+
+    while box !=[]:
+        node=box.pop(0)
+        expored_set.append(node)
+        if (node==goal):
+            print("fundet");
+            return
+        #lerada chayald war agretawa    
+        value=map.get(node,[])
+        for child in value:
+            if child not in box and child not in expored_set:
+                box.append(child)
+                print("try")
+bfs(5,8)
+
+
+
+
+
+
+
+
+map={
+    5: [3,7],
+    3:[2,4],
+    7:[8],
+    2:[],
+    4:[8],
+    8:[]#goal
+}
+def dfs(start,goal):
+    box=[start]
+    expored_set=[]
+
+    while box !=[]:
+        node=box.pop()
+        expored_set.append(node)
+        if (node==goal):
+            print("fundet");
+            return
+        #lerada chayald war agretawa    
+        value=reversed (map.get(node,[]))
+        for child in value:
+            if child not in box and child not in expored_set:
+                box.append(child)
+                print("try")
+bfs(5,8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
